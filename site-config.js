ba606debdf28a7fbc440e0e2578bc5d2fe8f1a74
Lucas Vogel <contact@vogelino.com>
2019-06-15 22:30:20 +0200

    Update social image, urls and icon

--- site-config.js
+++ site-config.js
@@ -4,13 +4,12 @@ module.exports = {
   siteTitle: 'Dominic Walch – Digital Strategy Consulting',
   siteTitleShort: 'Dominic Walch',
   siteDescription: 'Beratung zur Geschäftsentwicklung, im Bereich Unternehmensfinanzierung und zur Marketingstrategie',
-  siteUrl: 'https://gu.fabianschultz.com',
-  themeColor: '#000',
+  siteUrl: 'https://betteraskwalch.com',
+  themeColor: '#ff4b39',
   backgroundColor: '#fff',
   pathPrefix: null,
   logo: path.resolve(__dirname, 'src/images/icon.png'),
   social: {
-    twitter: 'gatsbyjs',
-    fbAppId: '966242223397117',
+    twitter: 'Waz_up_Dom',
   },
 };
