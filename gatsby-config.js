3d9bd57c7a7fd2fbc5afd21efaa4b82a89548217
Lucas Vogel <contact@vogelino.com>
2019-07-17 13:40:40 +0200

    Fix eslint warnings

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,24 +5,24 @@ module.exports = {
     ...siteConfig,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-offline`,
-    `gatsby-transformer-json`,
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-eslint`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
+    'gatsby-transformer-json',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-eslint',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `content`,
+        name: 'content',
         path: `${__dirname}/content`,
       },
     },
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-webpack-size`,
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-webpack-size',
     {
-      resolve: `gatsby-plugin-react-svg`,
+      resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
           include: /images\/.*\.svg$/,
--- gatsby-node.js
+++ gatsby-node.js
@@ -2,18 +2,24 @@ const path = require('path');
 const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
 
 exports.onCreateWebpackConfig = ({
+  // eslint-disable-next-line no-unused-vars
   stage,
+  // eslint-disable-next-line no-unused-vars
   getConfig,
+  // eslint-disable-next-line no-unused-vars
   rules,
+  // eslint-disable-next-line no-unused-vars
   loaders,
   actions,
 }) => {
   actions.setWebpackConfig({
     resolve: {
       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-      plugins: [new DirectoryNamedWebpackPlugin({
-        exclude: /node_modules/
-      })],
+      plugins: [
+        new DirectoryNamedWebpackPlugin({
+          exclude: /node_modules/,
+        }),
+      ],
     },
   });
 };
